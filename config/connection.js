2b15917cd6e462ac70ad7d732a4df7acb01c3717
Gabriel Guilherme <you@example.com>
2023-05-29 01:57:13 -0300

    adding connection.js

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const Sequelize = require('sequelize');
+
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = 'localhost';
+const DB_PORT = 3306;
+
+// Create the Sequelize instance
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: 'mysql',
+});
+
+// Test the database connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+module.exports = sequelize;
